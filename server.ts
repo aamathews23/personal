f988083bf4846057a4bb635ffff7998802397fe1
Aaron Mathews <aamathews23@gmail.com>
2025-06-02 13:43:52 -0700

    Feature/cloudflare deploy (#5)
    * convert project to use cloudflare
    
    * update action

---
+++ server.ts
@@ -0,0 +1,33 @@
+import { createRequestHandler, type ServerBuild } from '@remix-run/cloudflare';
+// eslint-disable-next-line import/no-unresolved
+import * as build from './build/server';
+import { getLoadContext } from 'load-context';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const handleRemixRequest = createRequestHandler(build as any as ServerBuild);
+
+export default {
+  async fetch(request, env, ctx) {
+    try {
+      const loadContext = getLoadContext({
+        request,
+        context: {
+          cloudflare: {
+            cf: request.cf,
+            ctx: {
+              props: ctx.props,
+              waitUntil: ctx.waitUntil.bind(ctx),
+              passThroughOnException: ctx.passThroughOnException.bind(ctx),
+            },
+            caches,
+            env,
+          },
+        },
+      });
+      return await handleRemixRequest(request, loadContext);
+    } catch (error) {
+      console.error(error);
+      return new Response('An unexpected error occurred', { status: 500 });
+    }
+  },
+} satisfies ExportedHandler<Env>;
