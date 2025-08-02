d82d0525d4164257b112d4ff0ab61c9b7c390f5b
Aaron Mathews <aamathews23@gmail.com>
2025-08-02 15:58:56 -0700

    small fixes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,12 +2,15 @@ import type { MetaFunction } from '@remix-run/cloudflare';
 import { Hero } from '@/components/Hero';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Welcome' }, { name: 'description', content: 'Welcome to my Remix App!' }];
+  return [
+    { title: 'Aaron Mathews' },
+    { name: 'description', content: 'Welcome to my website. I hope you enjoy your time here!' },
+  ];
 };
 
 export default function Index() {
   return (
-    <main className="mx-auto my-0 flex w-full max-w-[1440px] flex-col items-center justify-center gap-8 px-4 py-8 md:px-8 lg:px-16">
+    <main className="mx-auto my-0 flex w-full max-w-[1024px] flex-col items-center justify-center gap-8 px-4 py-8 md:px-8 lg:px-16">
       <Hero
         preheading="Welcome!"
         heading="I’m, Aaron Mathews."
