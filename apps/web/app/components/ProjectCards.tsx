8e8eff41806e9b5994530f864a476db9718a8b57
Aaron Mathews <aamathews23@gmail.com>
2025-09-28 12:39:47 -0700

    feat(project cards): add unit test for project cards component

--- apps/web/app/components/ProjectCards.tsx
+++ apps/web/app/components/ProjectCards.tsx
@@ -12,23 +12,39 @@ export type ProjectCardsProps = {
 
 export const ProjectCards = ({ heading, cards }: ProjectCardsProps) => {
   return (
-    <section className="flex w-full flex-col gap-8">
-      <h2 className="font-noto-serif text-heading-sm lg:text-heading-base font-bold text-slate-950">
+    <section
+      className="flex w-full flex-col gap-8"
+      data-testid="project-cards-section"
+    >
+      <h2
+        className="font-noto-serif text-heading-sm lg:text-heading-base font-bold text-slate-950"
+        data-testid="project-cards-heading"
+      >
         {heading}
       </h2>
-      <div className="grid w-full grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3">
+      <div
+        className="grid w-full grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3"
+        data-testid="project-cards-list"
+      >
         {cards.map((card) => (
           <article
             className="relative flex flex-col gap-4 rounded-lg border-2 border-slate-300 p-4 transition-colors hover:border-slate-500"
             key={card.id}
+            data-testid="project-card-card"
           >
             <Link
               className="font-roboto text-lg font-bold text-slate-950 after:absolute after:top-0 after:left-0 after:h-full after:w-full hover:underline"
               to={card.to}
+              data-testid="project-card-link"
             >
-              <h3>{card.heading}</h3>
+              <h3 data-testid="project-card-heading">{card.heading}</h3>
             </Link>
-            <p className="font-roboto text-base text-slate-950">{card.description}</p>
+            <p
+              className="font-roboto text-base text-slate-950"
+              data-testid="project-card-description"
+            >
+              {card.description}
+            </p>
           </article>
         ))}
       </div>
