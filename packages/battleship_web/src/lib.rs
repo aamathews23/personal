219a3a6c5805181982762523d420b5af73b1beac
Aaron Mathews <aamathews23@gmail.com>
2025-11-20 06:51:22 -0800

    fix: add panic hook to wasm functions

--- packages/battleship_web/src/lib.rs
+++ packages/battleship_web/src/lib.rs
@@ -5,15 +5,18 @@ use battleship;
 
 #[wasm_bindgen]
 pub fn build_board() -> Vec<u8> {
+    utils::set_panic_hook();
     battleship::utils::build()
 }
 
 #[wasm_bindgen]
 pub fn shoot_ship(idx: usize, board: &mut [u8]) -> u8 {
+    utils::set_panic_hook();
     battleship::utils::shoot(idx, board)
 }
 
 #[wasm_bindgen]
 pub fn is_end_of_game(board: &[u8]) -> bool {
+    utils::set_panic_hook();
     battleship::utils::is_end(board)
 }
